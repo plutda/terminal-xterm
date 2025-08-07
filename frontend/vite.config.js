0857f6e0397761fa2fa5d3fb43ebc2dc55f15c4d
pluto <691841335@qq.com>
2025-08-07 17:17:24 +0800

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_ACTIONS ? '/terminal-xterm/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
