09922b4c06e6b17fafa84cd0efb83e5d401170c9
pluto <691841335@qq.com>
2025-08-08 09:11:50 +0800

    init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
@@ -15,7 +14,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(createPinia())
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
